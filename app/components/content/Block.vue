a0ed6053d416b4677f8eb0d092577ccba56775ec
Antoine Lozach <antoine@MBP.local>
2025-11-10 15:08:44 +0100

    deps

--- app/components/content/Block.vue
+++ app/components/content/Block.vue
@@ -9,5 +9,25 @@ console.log("hello block")
 </template>
 
 <style scoped lang="scss">
+.block {
+  margin-top: 80px;
 
+  > .title {
+    font-weight: 700;
+    margin-bottom: 24px;
+  }
+
+  > .text {
+
+    margin-bottom: 16px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  > .list {
+    margin-bottom: 16px;
+  }
+}
 </style>
--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -11,8 +11,48 @@ if (!page.value) {
 </script>
 
 <template>
-  <ContentRenderer
-    v-if="page"
-    :value="page"
-  />
+  <div class="main">
+    <ContentRenderer
+      v-if="page"
+      :value="page"
+    />
+  </div>
 </template>
+
+<style lang="scss">
+.main {
+  padding: 0 20px;
+
+  @include media('>=tablet') {
+    padding: 0 124px;
+  }
+
+  @include media('>=desktop') {
+    padding: 0;
+    max-width: 446px;
+    margin: auto;
+  }
+
+  > .block {
+    margin-top: 80px;
+
+    > .title {
+      font-weight: 700;
+      margin-bottom: 24px;
+    }
+
+    > .text {
+
+      margin-bottom: 16px;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+
+    > .list {
+      margin-bottom: 16px;
+    }
+  }
+}
+</style>
